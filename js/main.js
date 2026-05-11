6acbd5f6bdfd521a587d7babc3c32d6a792f872b
Gul Faizi <gulahmadfaizi1987@gmail.com>
2026-05-11 09:23:45 -0400

    over all site design changed.

--- js/main.js
+++ js/main.js
@@ -122,71 +122,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // --- Particles Background (Simple Implementation) ---
-    const particlesContainer = document.getElementById('particles-js');
-    if (particlesContainer) {
-        // Create canvas
-        const canvas = document.createElement('canvas');
-        particlesContainer.appendChild(canvas);
-        const ctx = canvas.getContext('2d');
 
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
 
-        let particles = [];
-        const particleCount = 50;
-
-        class Particle {
-            constructor() {
-                this.x = Math.random() * canvas.width;
-                this.y = Math.random() * canvas.height;
-                this.size = Math.random() * 3 + 1;
-                this.speedX = Math.random() * 1 - 0.5;
-                this.speedY = Math.random() * 1 - 0.5;
-                this.color = `rgba(0, 243, 255, ${Math.random() * 0.5})`;
-            }
-
-            update() {
-                this.x += this.speedX;
-                this.y += this.speedY;
-
-                if (this.x > canvas.width || this.x < 0) this.speedX *= -1;
-                if (this.y > canvas.height || this.y < 0) this.speedY *= -1;
-            }
-
-            draw() {
-                ctx.fillStyle = this.color;
-                ctx.beginPath();
-                ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-                ctx.fill();
-            }
-        }
-
-        const init = () => {
-            particles = [];
-            for (let i = 0; i < particleCount; i++) {
-                particles.push(new Particle());
-            }
-        };
-
-        const animate = () => {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            particles.forEach(p => {
-                p.update();
-                p.draw();
-            });
-            requestAnimationFrame(animate);
-        };
-
-        init();
-        animate();
-
-        window.addEventListener('resize', () => {
-            canvas.width = window.innerWidth;
-            canvas.height = window.innerHeight;
-            init();
-        });
-    }
-
-    console.log('ZeerakAI Scripts Loaded');
+    console.log('ZeerakDev Scripts Loaded');
 });
